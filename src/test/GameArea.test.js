b1e4076bf5c5881e689555313ee063b387c22a05
kobesparrow <markcollins.md@gmail.com>
2019-04-15 13:47:11 -0600

    One working test

--- src/test/GameArea.test.js
+++ src/test/GameArea.test.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import GameArea from '../components/GameArea';
+import { shallow } from 'enzyme';
+
+
+let mockPrompt = {
+  "id": "44",
+  "Question": "What does the @onceler directive do",
+  "answers": ["repeats CSS definitions multiple times in a row",
+    "sets style based on a condition",
+    "@onceler is not a Sass directive"],
+  "correctAnswer": "@onceler is not a Sass directive"
+};
+let mockStoreWrongAnswer = jest.fn();
+let mockPopulatePrompt = jest.fn();
+let mockScoreIncrease = jest.fn();
 
 
 describe ('GameArea', () => {
@@ -7,10 +21,15 @@ describe ('GameArea', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <GameArea />
+      <GameArea 
+        prompt={ mockPrompt }
+        storeWrongAnswer={ mockStoreWrongAnswer }
+        populatePrompt={ mockPopulatePrompt }
+        scoreIncrease={ mockScoreIncrease }
+      />
     )
   });
-  it('should match the snapshot', () => {
+  it('should match the snapshot with all data passed in', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
